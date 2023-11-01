6345be9cd0ed020d462b7c89ca7f6d0270be572b
unknown <ra188897@ACAD.UCDB.BR>
2023-10-31 20:19:25 -0400

    Imprimindo Hello World

---
+++ HelloGit/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Function call
+        mensagem();
+    }
+
+    public static void mensagem(){
+        System.out.println("Hello, world!");
+    }
+}
